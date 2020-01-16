115869b9fca6bf327a82d6e29baa0e81cc22ee6c
ekapolwong <ekapolw@gmail.com>
2020-01-16 23:42:52 +0700

    Modify Domain Class to make it Success.

--- src/main/java/com/technicallab/courseservice/domain/CourseAdminDomain.java
+++ src/main/java/com/technicallab/courseservice/domain/CourseAdminDomain.java
@@ -10,7 +10,7 @@ public class CourseAdminDomain {
     String rtnMessage = "";
 
     if (sweetLevel.equals("NORMAL")) {
-      rtnMessage = "Admin Service is up na ka.";
+      rtnMessage = "Admin Service is up na.";
     } else if (sweetLevel.equals("SWEET")) {
       rtnMessage = "Admin Service is up na ja.";
     }
